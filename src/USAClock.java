4f2159be5c902c08e7fe7901bd3cd376a9da9d1e
Guilherme23645 <gui.depaula236@gmail.com>
2025-07-31 21:53:34 +0000

    feat: add USAClock

---
+++ src/USAClock.java
@@ -0,0 +1,20 @@
+public class USAClock extends Clock {
+    private String period;
+
+    public void setHour(int hour) {
+        if ((hour > 12) && (hour <= 23)) {
+            this.period = "PM";
+            hour = hour - 12;
+        } 
+
+        if (hour >= 24) {
+            this.period = "AM";
+            hour = 0;
+        } 
+    }
+
+    @Override
+    public Clock convert(final Clock clock) {
+        return null;
+    }
+}
