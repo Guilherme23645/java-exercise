4479d8b8fff6fb7e49755e187941d849987d023d
Guilherme23645 <gui.depaula236@gmail.com>
2025-07-31 22:10:34 +0000

    feat: setting up conversion method for BRLClock

--- src/BRLClock.java
+++ src/BRLClock.java
@@ -1,6 +1,19 @@
-public class BRLClock extends Clock{
+public non-sealed class BRLClock extends Clock{
     @Override
     public Clock convert(final Clock clock) {
+        switch (clock) {
+            case USAClock usaClock -> {
+                this.second = usaClock.getSecond();
+                this.minute = usaClock.getMinute();
+                this.hour = usaClock.getPeriod().equals("PM") ? usaClock.getHour() + 12 : usaClock.getHour() ;
+            }
+
+            case BRLClock brlClock -> {
+                this.second = brlClock.getSecond();
+                this.minute = brlClock.getMinute();
+                this.hour = brlClock.getHour();
+            }
+        }
         return null;
     }
 }
