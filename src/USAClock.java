f233543a34c1391522cd5142d9f67d00fb839261
Guilherme23645 <gui.depaula236@gmail.com>
2025-07-31 22:02:10 +0000

    feat: finishing setHour method

--- src/USAClock.java
+++ src/USAClock.java
@@ -5,20 +5,25 @@ public class USAClock extends Clock {
         return period;
     }
 
-    public void setPeriod(String period) {
-        this.period = period;
+    public void setAfterMidDay() {
+        this.period = "PM";
+    }
+
+    public void setBeforeMidDay() {
+        this.period = "AM";
     }
     
     public void setHour(int hour) {
+        setBeforeMidDay();
         if ((hour > 12) && (hour <= 23)) {
-            this.period = "PM";
-            hour = hour - 12;
-        } 
-
-        if (hour >= 24) {
-            this.period = "AM";
-            hour = 0;
-        } 
+            setAfterMidDay();
+            this.hour = hour - 12;
+        } else if (hour >= 24) {
+            setBeforeMidDay();
+            this.hour = 0;
+        } else {
+            this.hour = hour;
+        }
     }
 
     @Override
