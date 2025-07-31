29a62f2947a4db0c441cb0a0b6efe87c05bb41ec
Guilherme23645 <gui.depaula236@gmail.com>
2025-07-31 21:55:53 +0000

    feat: update USAClock attributes

--- src/USAClock.java
+++ src/USAClock.java
@@ -1,6 +1,14 @@
 public class USAClock extends Clock {
     private String period;
 
+    public String getPeriod() {
+        return period;
+    }
+
+    public void setPeriod(String period) {
+        this.period = period;
+    }
+    
     public void setHour(int hour) {
         if ((hour > 12) && (hour <= 23)) {
             this.period = "PM";
