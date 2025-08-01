a6f178f0249b30fe1d903b38cfdaf095bbba6be6
Guilherme23645 <gui.depaula236@gmail.com>
2025-08-01 21:50:44 +0000

    feat: add menu

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,47 @@
+import java.util.Scanner;
+
 public class Main {
+    private final static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println(new Circle(4).getArea());
-        System.out.println(new Square(4).getArea());
-        System.out.println(new Rectangle(4,6).getArea());
+        int choice;
+        do {
+            System.out.println("Escolha a forma geométrica para calcular a área");
+            System.out.println("1. Quadrado");
+            System.out.println("2. Retângulo");
+            System.out.println("3. Círculo");
+            System.out.println("4. Sair");
+
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1 -> squareArea();
+                case 2 -> rectangleArea();
+                case 3 -> circleArea();
+            }
+        } while (choice != 0);
+        scanner.close();
+    }
+
+    public static void squareArea() {
+        System.out.println("Informe o tamanho do lado do quadrado:");
+        double side = scanner.nextDouble();
+        Shape square = new Square(side);
+        System.out.println(square.getArea());
+    }
+
+    public static void rectangleArea() {
+        System.out.println("Informe o comprimento do retângulo:");
+        double width = scanner.nextDouble();
+        System.out.println("Informe a altura do retângulo:");
+        double height = scanner.nextDouble();
+        Shape rectangle = new Rectangle(width, height);
+        System.out.println(rectangle.getArea());
+    }
+
+    public static void circleArea() {
+        System.out.println("Informe o tamanho do lado do quadrado:");
+        double radius = scanner.nextDouble();
+        Shape circle = new Circle(radius);
+        System.out.println(circle.getArea());
     }
 }
